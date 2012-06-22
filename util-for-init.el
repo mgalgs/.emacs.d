e052dda847caf42460b218c904c5a0e408c252cc
Mitchel Humpherys <mitch.special@gmail.com>
2012-06-21 22:44:19 -0700

    removing some debug prints

--- util-for-init.el
+++ util-for-init.el
@@ -7,8 +7,6 @@
                             (directory-files directory)))
          ;; remove '.', '..' and all non-directories
          (filtered-files (remove-if (lambda (el)
-                                      (message "testing %s (last char is %s)" el (substring el -1))
-                                      (if (file-directory-p el) (message "%s is a dir" el))
                                       (or (string= "." (substring el -1))
                                           (not (file-directory-p el))))
                                     all-files)))
