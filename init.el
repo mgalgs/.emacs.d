73b9b2ae93c5ea18b44ec761a77b10a396183bfc
Mitchel Humpherys <mitch.special@gmail.com>
2013-02-27 21:26:40 -0800

    use spaces instead of tabs

--- init.el
+++ init.el
@@ -94,12 +94,11 @@
  isearch-allow-scroll t)
 
 (setq-default
- ;; indent-tabs-mode nil ; don't use the tab character, only spaces
+ indent-tabs-mode nil ; don't use the tab character, only spaces
  c-basic-offset 8 ; how many spaces our tab key will insert
  tab-width 8 ; default tab-width (when there are existing tabs in files)
  ;; tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120)
  ;; tab-stop-list '(8 16 24 32 40 48 56 64 72 80 88 96 104 112 120)
- indent-tabs-mode t ; use the tab character, not spaces
  set-mark-command-repeat-pop t ; repeated C-SPC after C-u C-SPC keeps popping the mark
  ;; c-default-style "bsd" ; for nasty brace face
  )
