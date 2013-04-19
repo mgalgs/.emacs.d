2c9872c1746f5d5e2c87ddef1291fa2688b3fb29
Mitchel Humpherys <mitch.special@gmail.com>
2013-04-19 16:24:42 -0700

    util: my-isearch-word-at-point

--- my-util.el
+++ my-util.el
@@ -602,3 +602,27 @@ in each `nnmail-split-methods'"
   directory named `WHAT'."
   (interactive)
   (my-do-find-parent-dir-that-contains what (file-name-directory (buffer-file-name))))
+
+(defun my-isearch-word-at-point ()
+  (interactive)
+  (call-interactively 'isearch-forward-regexp))
+
+(defun my-isearch-yank-word-hook ()
+  (when (equal this-command 'my-isearch-word-at-point)
+    (let ((string (concat "\\<"
+                          (buffer-substring-no-properties
+                           (progn (skip-syntax-backward "w_") (point))
+                           (progn (skip-syntax-forward "w_") (point)))
+                          "\\>")))
+      (if (and isearch-case-fold-search
+               (eq 'not-yanks search-upper-case))
+          (setq string (downcase string)))
+      (setq isearch-string string
+            isearch-message
+            (concat isearch-message
+                    (mapconcat 'isearch-text-char-description
+                               string ""))
+            isearch-yank-flag t)
+      (isearch-search-and-update))))
+
+(add-hook 'isearch-mode-hook 'my-isearch-yank-word-hook)
