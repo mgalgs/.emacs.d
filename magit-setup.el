11e98d47eef159a54bef4eae7d3840d62744ccb4
Mitchel Humpherys <mitch.special@gmail.com>
2013-06-14 14:33:48 -0700

    magit-setup: don't let the history fall off the repo

--- magit-setup.el
+++ magit-setup.el
@@ -8,10 +8,17 @@ whichever is less"
                                                                        nrevs)))))
     (min nrevs have-revs)))
 
+(defun my-git-get-rev-nrevs-back (nrevs)
+  "Returns the ref back `nrevs' (or less if so determined by
+`my-git-nrevs-gate')"
+  (substring (shell-command-to-string (format "git rev-parse HEAD~%d"
+                                              (1- (my-git-nrevs-gate nrevs))))
+             0 -1))
+
 (defun my-git-get-log-range ()
-  "Get something like HEAD~100..HEAD (with the `100' sanitized to
+  "Get something like HEAD~100.. (with the `100' sanitized to
 only go back as far as the git history) to pass to magit-log."
-  (format "HEAD~%d..HEAD"
-          (my-git-nrevs-gate 100)))
+  (format "%s.."
+          (my-git-get-rev-nrevs-back 100)))
 
 (setq magit-log-default-log-range 'my-git-get-log-range)
