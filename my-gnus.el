d4ad3208d68a0960c0ce6a75ce817aee4fe408a8
Mitchel Humpherys <mitch.special@gmail.com>
2013-10-09 20:52:13 -0700

    gnus: fancy threading and whatnot
    Change-Id: Id26d2a4d59bbb3b7c3a5b1b9ba97c171d5863d82

--- my-gnus.el
+++ my-gnus.el
@@ -17,3 +17,22 @@
 
 (setq user-mail-address "mitch.special@gmail.com")
 (setq user-full-name "Mitchel Humpherys")
+(setq gnus-permanently-visible-groups "INBOX")
+
+(setq-default
+ gnus-summary-line-format "%U%R%z %(%&user-date;  %-15,15f  %B%s%)\n"
+ gnus-user-date-format-alist '((t . "%Y-%m-%d %H:%M"))
+ gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
+ gnus-thread-sort-functions '(gnus-thread-sort-by-date))
+
+(setq message-confirm-send t)
+
+;; eye-candy for the summary view
+(when window-system
+  (setq gnus-sum-thread-tree-indent "  ")
+  (setq gnus-sum-thread-tree-root "● ")
+  (setq gnus-sum-thread-tree-false-root "◯ ")
+  (setq gnus-sum-thread-tree-single-indent "◎ ")
+  (setq gnus-sum-thread-tree-vertical        "│")
+  (setq gnus-sum-thread-tree-leaf-with-other "├─► ")
+  (setq gnus-sum-thread-tree-single-leaf     "╰─► "))
