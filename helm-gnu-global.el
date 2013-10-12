5731b0a1b458743184cad564c868cc08937e3021
Mitchel Humpherys <mitch.special@gmail.com>
2013-10-11 17:42:56 -0700

    helm-gnu-global: actually populate the list of symbols

--- helm-gnu-global.el
+++ helm-gnu-global.el
@@ -1,11 +1,26 @@
+(defvar helm-gnu-global-global-command nil
+  "Command name of global.")
+
+;; Set global's command name
+(setq helm-gnu-global-global-command
+      (getenv "GTAGSGLOBAL"))
+(if (or (not helm-gnu-global-global-command)
+        (equal helm-gnu-global-global-command ""))
+    (setq helm-gnu-global-global-command
+          "global"))
+
 (defun helm-gnu-global-get-symbols ()
-  '(("a" . "a")
-    ("b" . "b")
-    ("c" . "c")))
+  (with-temp-buffer
+    (call-process helm-gnu-global-global-command nil t nil "-c")
+    (split-string (buffer-string))))
+
+(defun helm-gnu-global-visit (el)
+  (message "got %s" el))
 
 (setq helm-source-gnu-global-symbols
       '((name . "Gnu Global Symbols")
-        (candidates . helm-gnu-global-get-symbols)))
+        (candidates . helm-gnu-global-get-symbols)
+        (action . (("Visit" . helm-gnu-global-visit)))))
 
 (defun helm-gnu-global ()
   (interactive)
