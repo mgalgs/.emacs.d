f10e9be956099f640c98b09c4f42331c10c83b1d
Mitchel Humpherys <mitch.special@gmail.com>
2013-10-23 21:46:50 -0700

    jedi-setup: more notes on setup

--- jedi-setup.el
+++ jedi-setup.el
@@ -1,4 +1,9 @@
 ;; Note to future self: if jedi isn't working on a new setup, make
 ;; sure you've done everything in site-lisp/emacs-jedi/README.rst
+;;
+;; Namely:
+;;    make requirements # (from the jedi directory)
+
+
 (require 'jedi)
 (add-hook 'python-mode-hook 'jedi:ac-setup)
