d8692b2fb28ee5801d33bfbac2cf3f13aa6d63bb
Mitchel Humpherys <mitch.special@gmail.com>
2013-11-12 10:05:45 -0800

    smartparens: fix some annoyances

--- smartparens-setup.el
+++ smartparens-setup.el
@@ -2,3 +2,12 @@
 (setq sp-navigate-reindent-after-up nil)
 (smartparens-global-mode 1)
 (show-smartparens-global-mode 1)
+
+;; pair "`" with "'" in emacs-lisp-mode
+(sp-local-pair 'emacs-lisp-mode "`" "'")
+;; no '' pair in emacs-lisp-mode
+(sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+
+;; don't pair "'" if we're at the end of a word (like when typing an
+;; apostrophe)
+(sp-pair "'" nil :unless '(sp-point-after-word-p))
