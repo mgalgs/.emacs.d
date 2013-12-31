d369e354c78250f2da89599cb9c04b96d4d9f7f2
Mitchel Humpherys <mitch.special@gmail.com>
2013-12-30 21:06:04 -0800

    ido-setup.el: disable auto-search thing

--- ido-setup.el
+++ ido-setup.el
@@ -4,6 +4,7 @@
 (setq ido-enable-flex-matching t) ; fuzzy matching
 (setq scroll-step 1)			;scroll step at top/bottom of page
 (setq ido-max-prospects 6)
+(setq ido-auto-merge-work-directories-length -1) ; Don't try to find files nearby ("Searching for...")
 (setq ido-ignore-buffers ;; ignore these guys
       '("\\` " "^\*Back"
         ".*Completion" "^\*Ido" "^\*trace"
