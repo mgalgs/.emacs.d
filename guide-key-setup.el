e577152d3da10bc5f4f2034acd3ed91cb92aa4fe
Mitchel Humpherys <mitch.special@gmail.com>
2014-01-13 15:24:44 -0800

    guide-key: add "C-x C-k" to guide-key/guide-key-sequence
    Keyboard macro stuff

--- guide-key-setup.el
+++ guide-key-setup.el
@@ -1,3 +1,8 @@
 (require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-c m" "C-c m m"))
+(setq guide-key/guide-key-sequence
+      '("C-x r"
+        "C-x 4"
+        "C-c m"
+        "C-c m m"
+        "C-x C-k"))
 (guide-key-mode 1)
