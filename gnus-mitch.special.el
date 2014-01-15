2ca7c2759fc4bab4160f093d65860241cf3464f6
Mitchel Humpherys <mitch.special@gmail.com>
2014-01-14 17:01:33 -0800

    gnus: add gmail setup file
    To be used like so:
    
        emacs --eval '(load-file "~/.emacs.d/gnus-mitch.special.el")'

---
+++ gnus-mitch.special.el
@@ -0,0 +1,7 @@
+(setq gnus-startup-file "~/.emacs.d/newsrc-mitch.special"
+      gnus-init-file "~/.emacs.d/my-gnus.el"
+      gnus-site-init-file nil
+      gnus-save-newsrc-file nil
+      gnus-read-newsrc-file nil
+      gnus-save-killed-list nil)
+(gnus)
