dc276b6d24a8ba32f6f41a23a0da22ae7fbb1106
Mitchel Humpherys <mitch.special@gmail.com>
2014-01-21 15:46:49 -0800

    ack-and-a-half: new commits for running ack with custom args

--- ack-and-a-half-setup.el
+++ ack-and-a-half-setup.el
@@ -4,3 +4,4 @@
 (defalias 'ack-same 'ack-and-a-half-same)
 (defalias 'ack-find-file 'ack-and-a-half-find-file)
 (defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
+(defalias 'ack-with-args 'ack-and-a-half-with-args)
