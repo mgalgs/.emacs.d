6411d3e44a23c72d86699b9353a6d3257196bcd8
Mitchel Humpherys <mitch.special@gmail.com>
2014-01-21 15:13:46 -0800

    smartparens: disable C-M-e

--- smartparens-setup.el
+++ smartparens-setup.el
@@ -50,7 +50,7 @@
 (define-key sp-keymap (kbd "C-S-a") 'sp-beginning-of-sexp)
 (define-key sp-keymap (kbd "C-S-d") 'sp-end-of-sexp)
 
-(define-key sp-keymap (kbd "C-M-e") 'sp-up-sexp)
+;; (define-key sp-keymap (kbd "C-M-e") 'sp-up-sexp)
 (define-key emacs-lisp-mode-map (kbd ")") 'sp-up-sexp)
 (define-key sp-keymap (kbd "C-M-u") 'sp-backward-up-sexp)
 (define-key sp-keymap (kbd "C-M-t") 'sp-transpose-sexp)
