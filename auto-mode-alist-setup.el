83b75d61c1560a936e5f6655fd26d36218236683
Mitchel Humpherys <mitch.special@gmail.com>
2014-02-06 12:24:51 -0800

    auto-mode-alist-setup.el: .gitconfig => conf-mode

--- auto-mode-alist-setup.el
+++ auto-mode-alist-setup.el
@@ -30,3 +30,4 @@
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("/.gitconfig". conf-mode))
