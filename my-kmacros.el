4e5b8329b707f6f1ec40d1e968e36bf306fd1a4f
Mitchel Humpherys <mitch.special@gmail.com>
2014-02-12 23:50:38 -0800

    my-kmacros.el: org-clock-to-hourrow (convert org clock to latex \hourrow)
    For invoicing.
    
    Change-Id: I88302f82135509c00ba6cc4142441e51323c6e8c

--- my-kmacros.el
+++ my-kmacros.el
@@ -6,3 +6,7 @@
 (fset 'kmy-cleanup-includes
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217848 115 101 97 114 99 104 45 102 111 114 119 tab 45 114 101 tab return 35 105 110 99 108 117 100 101 46 42 91 34 62 backspace 60 93 46 42 47 46 42 91 34 62 93 43 2 2 2 2 2 2 2 2 2 2 43 5 46 42 36 return 134217848 115 101 97 114 99 104 45 98 97 99 107 tab 45 114 101 tab return 91 34 62 93 43 return 18 47 13 6 67108896 134217848 115 101 97 114 99 104 45 98 97 99 107 tab 45 114 101 tab return 91 34 60 93 43 return 6 23 5 6] 0 "%d")) arg)))
 
+
+(fset 'org-clock-to-hourrow
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([tab 92 104 111 117 114 114 111 119 67108896 19 91 return backspace 67108896 19 32 return 2 23 123 25 125 67108896 19 61 62 return backspace 134217820 67108896 5 23 123 21 134217786 40 102 111 114 109 97 116 32 34 37 46 50 102 34 32 40 25 67108911 109 121 45 104 111 117 114 tab 32 34 25 34 41 41 return backspace 5 134217730 6 4 5 6] 0 "%d")) arg)))
+
