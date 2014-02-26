60530278d19307eb44a4f1f399e6705c141273f5
Mitchel Humpherys <mitch.special@gmail.com>
2014-02-26 14:29:15 -0800

    auto-mode-alist-setup.el: Kbuild => makefile-mode

--- auto-mode-alist-setup.el
+++ auto-mode-alist-setup.el
@@ -27,6 +27,7 @@
 (add-to-list 'auto-mode-alist '("/Kconfig$" . kconfig-mode))
 (add-to-list 'auto-mode-alist '("/Kconfig\\..*$" . kconfig-mode))
 (add-to-list 'auto-mode-alist '("_defconfig$" . conf-mode))
+(add-to-list 'auto-mode-alist '("/Kbuild$" . makefile-mode))
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml?\\'" . web-mode))
