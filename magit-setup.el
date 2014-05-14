100a71d3e10566dc90f7a13e32cca9e7d6d11fc9
Mitchel Humpherys <mitch.special@gmail.com>
2014-05-13 22:42:38 -0700

    magit-setup.el: add `magit-push-dwis'
    This is my local version of magit/magit#1362
    
    A common workflow is to create a long-lived development branch (that
    you'd like to publish remotely) while always rebasing to and monitoring
    some other upstream branch. With vanilla git, the push.default config
    can be set to "current" to achieve this. However, with
    `magit-push-dwim', there's no way to do with without using a prefix arg
    (which is extra work).
    
    Add a new push "backend", `magit-push-dwis', that behaves exactly like
    `magit-push-dwim' except it never specifies a remote branch to push to
    unless explicitly given with a prefix arg. Without the explicit remote
    branch, the behavior is determined by the config push.default.

--- magit-setup.el
+++ magit-setup.el
@@ -49,6 +49,32 @@ only go back as far as the git history) to pass to magit-log."
       (my-git-get-log-range 15)
       (format "--max-count=%d" 15))))
 
+(defun magit-push-dwis (arg)
+  "Like `magit-push-dwim' but doesn't mess with setting upstream
+branches or push to branch.<name>.merge by default. The goal here
+is to respect the config push.default. If push.default=current
+you really want to push to the remote branch of the same name as
+the local branch, even if your
+upstream (i.e. branch.<name>.merge) is set to something else."
+  (interactive "P")
+  (let* ((branch (or (magit-get-current-branch)
+                     (user-error "Don't push a detached head.  That's gross")))
+         (auto-remote (magit-get-remote branch))
+         (used-remote (if (or arg (not auto-remote))
+                          (magit-read-remote
+                           (format "Push %s to remote" branch) auto-remote)
+                        auto-remote))
+         (used-branch (when (>= (prefix-numeric-value arg) 16)
+                        (magit-read-remote-branch
+                         (format "Push %s as branch" branch)
+                         used-remote))))
+    (magit-run-git-async
+     "push" "-v" used-remote
+     (if used-branch (format "%s:%s" branch used-branch) branch)
+     magit-custom-options)))
+
+(setq magit-push-hook 'magit-push-dwis)
+
 (setq magit-status-sections-hook
       '(magit-insert-status-local-line
         magit-insert-status-remote-line
