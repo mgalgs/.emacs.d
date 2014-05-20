b05a9d5621a5af344ae706c0484e0786f2d9cfc0
Mitchel Humpherys <mitch.special@gmail.com>
2014-05-19 22:21:34 -0700

    magit-setup.el: move to built-in recent-log inserter

--- magit-setup.el
+++ magit-setup.el
@@ -1,53 +1,10 @@
 (require 'magit)
 
-(defun my-git-nrevs (max-count)
-  (1- (length (split-string (shell-command-to-string (format "git rev-list --max-count=%s HEAD"
-                                                             max-count))
-                            "\n"))))
-
-(defun my-git-nrevs-gate (nrevs)
-  "Returns `nrevs' or `git rev-parse --max-count=num HEAD | wc -l',
-whichever is less. This is useful to prevent oneself from falling
-off the beginning of a git history."
-  (let* ((have-revs (my-git-nrevs nrevs)))
-    (min nrevs have-revs)))
-
-(defun my-git-get-rev-nrevs-back (nrevs)
-  "Returns the ref back `nrevs' (or less if so determined by
-`my-git-nrevs-gate')"
-  (substring (shell-command-to-string (format "git rev-parse HEAD~%d"
-                                              (1- (my-git-nrevs-gate nrevs))))
-             0 -1))
-
-(defun my-git-get-log-range (n)
-  "Get something like HEAD~n.. (with the `n' sanitized to
-only go back as far as the git history) to pass to magit-log."
-  (if (= n (my-git-nrevs n))
-      ;; there's at least n commits. Let's limit with .. for
-      ;; performance.
-      (format "%s.." (my-git-get-rev-nrevs-back n))
-    "HEAD"))
-
-;; (setq magit-log-default-log-range 'my-git-get-log-range)
 (setq magit-diff-refine-hunk 'all)
 (setq git-rebase-auto-advance t)
 (setq magit-stage-all-confirm nil)
 (setq magit-commit-squash-commit 'marked-or-current)
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
-
-(defun my-magit-insert-recent-log ()
-  ;; doesn't work if there aren't any commits yet
-  (when (= 0 (call-process "git" nil nil nil "rev-parse" "HEAD"))
-    (magit-git-insert-section (recent "Recent log:")
-        (lambda ()
-          (ansi-color-apply-on-region (point-min) (point-max)))
-      "log"
-      "--graph"
-      "--oneline"
-      "--decorate"
-      "--color"
-      (my-git-get-log-range 15)
-      (format "--max-count=%d" 15))))
 
 (defun magit-push-dwis (arg)
   "Like `magit-push-dwim' but doesn't mess with setting upstream
@@ -89,7 +46,7 @@ upstream (i.e. branch.<name>.merge) is set to something else."
         magit-insert-untracked-files
         magit-insert-unpushed-cherries
         magit-insert-unpulled-cherries
-        my-magit-insert-recent-log
+        magit-insert-recent-commits-graph
         magit-insert-stashes))
 
 (font-lock-add-keywords 'emacs-lisp-mode
