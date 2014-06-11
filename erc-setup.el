79f298d357b203895e694e917ee1983165003eea
Mitchel Humpherys <mitch.special@gmail.com>
2014-06-11 10:30:17 -0700

    erc-setup.el: only do region stuff when region-active-p

--- erc-setup.el
+++ erc-setup.el
@@ -1,5 +1,6 @@
 (defun my-erc-ansi-colors ()
   "For ansi color escape sequences"
-  (ansi-color-apply-on-region (region-beginning) (region-end)))
+  (when (region-active-p)
+    (ansi-color-apply-on-region (region-beginning) (region-end))))
 
 (add-hook 'erc-insert-modify-hook 'my-erc-ansi-colors)
