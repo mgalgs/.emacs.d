264d53b45ce77afb5dfc09589478b0c10b6e73b6
Mitchel Humpherys <mitch.special@gmail.com>
2014-07-24 13:10:16 -0700

    auto-mode-alist-setup.el: .dtsi? => dts-mode

--- auto-mode-alist-setup.el
+++ auto-mode-alist-setup.el
@@ -36,3 +36,5 @@
 (add-to-list 'auto-mode-alist '("/.gitconfig". conf-mode))
 (add-to-list 'auto-mode-alist '("\.zsh" . sh-mode))
 (add-to-list 'auto-mode-alist '("\.install" . sh-mode))
+
+(add-to-list 'auto-mode-alist '("\.dtsi?" . dts-mode))
