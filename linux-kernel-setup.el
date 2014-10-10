d4a910285a58439ac0aa7decd1b458a9d27aa05c
Mitchel Humpherys <mitch.special@gmail.com>
2014-10-09 21:58:43 -0700

    linux-kernel-setup: break hook function out
    Change-Id: If4231523953876f0df07f066d9dd89610cee78ea

--- linux-kernel-setup.el
+++ linux-kernel-setup.el
@@ -17,16 +17,19 @@
                          c-lineup-gcc-asm-reg
                          c-lineup-arglist-tabs-only))))))
 
-(add-hook 'c-mode-hook
-          (lambda ()
-            (let ((filename (buffer-file-name)))
-              ;; Enable kernel mode for the appropriate files
-              (when (and filename
-                         (or (string-match (expand-file-name "/local/mnt/workspace/mitchelh/.*/kernel")
-                                           filename)
-                             (string-match "/local/mnt/workspace/mitchelh/msm-kvm"
-                                           filename)
-                             (locate-dominating-file filename "Kbuild")))
-                (setq indent-tabs-mode t)
-                (message "Setting up indentation for the linux kernel")
-                (c-set-style "linux-tabs-only")))))
+(defun my-kernel-source-hook ()
+  (let ((filename (buffer-file-name)))
+    ;; Enable kernel mode for the appropriate files
+    (when (and filename
+               (or (string-match (expand-file-name "/local/mnt/workspace/mitchelh/.*/kernel")
+                                 filename)
+                   (string-match "/local/mnt/workspace/mitchelh/msm-kvm"
+                                 filename)
+                   (locate-dominating-file filename "Kbuild")
+                   (save-excursion (goto-char 0)
+                                   (search-forward-regexp "^#include <linux/\\(module\\|kernel\\)\\.h>$"))))
+      (setq indent-tabs-mode t)
+      (message "Setting up indentation for the linux kernel")
+      (c-set-style "linux-tabs-only"))))
+
+(add-hook 'c-mode-hook 'my-kernel-source-hook)
