97c773413bc983af1445768afcc3c31353188d11
Mitchel Humpherys <mitch.special@gmail.com>
2014-10-21 09:58:56 -0700

    eldoc: show eldoc in modeline when using M-:

--- eldoc-setup.el
+++ eldoc-setup.el
@@ -1,3 +1,4 @@
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 (add-hook 'lisp-interaction-mode 'eldoc-mode)
 (add-hook 'ielm-mode-hook 'eldoc-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'eldoc-mode)
