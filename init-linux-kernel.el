a3e7ab8e2af047c273e91fb72486c8e9bc28662b
Mitchel Humpherys <mitch.special@gmail.com>
2015-09-19 08:31:34 -0700

    init.el: Don't add "linux-tabs-only" style in c-mode-common-hook
    Change-Id: Ifda1dc28a173ad93697cd24e5a894116c280e671

--- init-linux-kernel.el
+++ init-linux-kernel.el
@@ -7,17 +7,13 @@
     (* (max steps 1)
        c-basic-offset)))
 
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            ;; Add kernel style
-            (c-add-style
-             "linux-tabs-only"
+(c-add-style "linux-tabs-only"
              '("linux" (c-offsets-alist
                         (arglist-cont-nonempty
                          c-lineup-gcc-asm-reg
-                         c-lineup-arglist-tabs-only))))))
+                         c-lineup-arglist-tabs-only))))
 
-(defun my-kernel-source-hook ()
+(defun m/kernel-source-hook ()
   (let ((filename (buffer-file-name)))
     ;; Enable kernel mode for the appropriate files
     (when (and filename
@@ -29,10 +25,10 @@
                    (locate-dominating-file filename "Kconfig")
                    (save-excursion (goto-char 0)
                                    (search-forward-regexp "^#include <linux/\\(module\\|kernel\\)\\.h>$" nil t))))
-      ;; (setq indent-tabs-mode t)
-      ;; (setq c-basic-offset 8)
-      ;; (setq tab-width 8)
+      (setq indent-tabs-mode t)
+      (setq tab-width 8)
+      (setq c-basic-offset 8)
       (message "Setting up indentation for the linux kernel")
       (c-set-style "linux"))))
 
-(add-hook 'c-mode-hook 'my-kernel-source-hook)
+(add-hook 'c-mode-hook 'm/kernel-source-hook)
