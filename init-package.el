7e6bd55e15ea1c93428e29c40e97f5a4efe92060
Mitchel Humpherys <mitch.special@gmail.com>
2015-09-28 21:59:59 -0700

    init-package.el: Disable marmalade repo
    Seems that everything I need is in MELPA...
    
    Change-Id: Ib8274da7c876ca813d87c4c7257eb6919a7309e5

--- init-package.el
+++ init-package.el
@@ -5,7 +5,7 @@
 (setq use-package-always-ensure t)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 
