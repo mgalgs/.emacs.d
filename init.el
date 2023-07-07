47b926a40f87f9d1fc20e3329b40cfbe3cd424dd
Mitchel Humpherys <mitch.special@gmail.com>
2023-07-06 17:14:38 -0700

    init.el: Add "merge" operation to ediff

--- init.el
+++ init.el
@@ -925,6 +925,17 @@ installed/loaded.")
 
 (use-package sudo-edit)
 
+;;; Add a "merge" operation to ediff
+;;; https://stackoverflow.com/a/29757750/209050
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+
 
 ;;; These lines should be last:
 ;; some keybindings
