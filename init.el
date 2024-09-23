a0b122993e8d1c4876b0cf80003237e9410c73bd
Mitchel Humpherys <mitch.special@gmail.com>
2024-09-23 16:56:17 -0700

    init.el: indent-bars: Initial

--- init.el
+++ init.el
@@ -1049,6 +1049,22 @@ eslint command line args with -c"
 (use-package gptel-quick
   :straight (gptel-quick :type git :host github :repo "karthink/gptel-quick"))
 
+(use-package indent-bars
+  :config
+  (require 'indent-bars-ts) 		; not needed with straight
+  :custom
+  (indent-bars-treesit-support t)
+  (indent-bars-treesit-ignore-blank-lines-types '("module"))
+  ;; Add other languages as needed
+  (indent-bars-treesit-scope '((python function_definition class_definition for_statement
+	                               if_statement with_statement while_statement)))
+  ;; Note: wrap may not be needed if no-descend-list is enough
+  ;;(indent-bars-treesit-wrap '((python argument_list parameters ; for python, as an example
+  ;;				      list list_comprehension
+  ;;				      dictionary dictionary_comprehension
+  ;;				      parenthesized_expression subscript)))
+  :hook ((python-base-mode yaml-mode) . indent-bars-mode))
+
 
 ;;; These lines should be last:
 ;; some keybindings
