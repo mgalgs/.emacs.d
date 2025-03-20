f7f2ddaeeeade04daca12acb095daa4dd91331aa
Mitchel Humpherys <mitch.special@gmail.com>
2025-03-20 13:10:40 -0700

    init.el: Set require-final-newline to t

--- init.el
+++ init.el
@@ -121,7 +121,8 @@ installed/loaded.")
       auto-revert-verbose nil
       mouse-wheel-mode nil
       confirm-kill-emacs 'y-or-n-p
-      visual-line-fringe-indicators '(right-arrow nil))
+      visual-line-fringe-indicators '(right-arrow nil)
+      require-final-newline t)
 
 (setq-default indent-tabs-mode nil ; don't use the tab character, only spaces
               ;; tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120)
