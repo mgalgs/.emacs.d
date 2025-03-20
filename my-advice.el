34516cb1775187aa1fd20d0a367e7b90257cce5c
Mitchel Humpherys <mitch.special@gmail.com>
2025-03-20 12:36:16 -0700

    my-advice.el: Trash some advice (was messing up Dart mode)
    Also, we don't use smartparens anymore, so that advice can be deleted.

--- my-advice.el
+++ my-advice.el
@@ -1,13 +1,8 @@
 ;; set mark when we go backward-up-list:
-(defadvice backward-up-list (around my-backward-up-list-advice activate)
-  "stuffs"
-  (push-mark (point) t)
-  ad-do-it)
-
-(defadvice sp-backward-up-sexp (around my-sp-backward-up-sexp-advice activate)
-  "stuffs"
-  (push-mark (point) t)
-  ad-do-it)
+;; (defadvice backward-up-list (around my-backward-up-list-advice activate)
+;;   "stuffs"
+;;   (push-mark (point) t)
+;;   ad-do-it)
 
 ;; (defadvice article-fill-long-lines (around my-article-fill-long-lines-advice activate)
 ;;   "override `fill-column' due to the way article-fill-long-lines
