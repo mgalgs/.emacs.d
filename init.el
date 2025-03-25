6c4c68aa56e76cfafb075f16c777a6961855cfef
Mitchel Humpherys <mitch.special@gmail.com>
2025-03-24 20:51:00 -0700

    init.el: treesit: Initial

--- init.el
+++ init.el
@@ -1098,6 +1098,42 @@ eslint command line args with -c"
 
 (add-hook 'compilation-mode-hook 'm/compilation-mode-hook)
 
+(use-package treesit
+  :ensure nil  ; it's built-in
+  :mode (("\\.ts" . typescript-ts-mode))
+  :preface
+                                        ; https://github.com/mickeynp/combobulate?tab=readme-ov-file#complete-example-with-tree-sitter-grammar-installation
+  (defun m/setup-install-grammars ()
+    "Install Tree-sitter grammars if they are absent."
+    (interactive)
+    (dolist (grammar
+             ;; Note the version numbers. These are the versions that
+             ;; are known to work with Combobulate *and* Emacs.
+             '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+               (go . ("https://github.com/tree-sitter/tree-sitter-go" "v0.20.0"))
+               (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+               (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1" "src"))
+               (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+               (markdown . ("https://github.com/ikatyang/tree-sitter-markdown" "v0.7.1"))
+               (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+               (rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
+               (toml . ("https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1"))
+               (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+               (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
+      (add-to-list 'treesit-language-source-alist grammar)
+      ;; Only install `grammar' if we don't already have it
+      ;; installed. However, if you want to *update* a grammar then
+      ;; this obviously prevents that from happening.
+      (unless (treesit-language-available-p (car grammar))
+        (message "Installing missing treesit grammar %s from %s @ %s"
+                 (car grammar)
+                 (cadr grammar)
+                 (cddr grammar))
+        (treesit-install-language-grammar (car grammar)))))
+  :config
+  (m/setup-install-grammars))
+
 
 ;;; These lines should be last:
 ;; some keybindings
