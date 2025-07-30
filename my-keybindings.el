f0af040dd97c6bab9239189e35c7b16246b01b88
Mitchel Humpherys <mitch.special@gmail.com>
2025-07-29 20:41:48 -0700

    keybindings: Name our gptel *OpenRouter*

--- my-keybindings.el
+++ my-keybindings.el
@@ -78,5 +78,5 @@
 (global-set-key (kbd "<f10>") 'delete-window)
 (global-set-key (kbd "<f1>") 'delete-other-windows)
 (global-set-key (kbd "C-c m v") 'visual-line-mode)
-(global-set-key (kbd "C-c m G") 'gptel)
+(global-set-key (kbd "C-c m G") (lambda () (interactive) (switch-to-buffer (gptel "*OpenRouter*"))))
 (global-set-key (kbd "M-x") 'helm-M-x)
