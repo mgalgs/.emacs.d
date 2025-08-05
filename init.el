801b24f2fb79f6b5168a0b13f79474a017412df2
Mitchel Humpherys <mitch.special@gmail.com>
2025-08-05 15:41:05 -0700

    init.el: jtsx: Set js-indent-level to 4

--- init.el
+++ init.el
@@ -1186,7 +1186,7 @@ eslint command line args with -c"
          (jtsx-typescript-mode . hs-minor-mode))
   :custom
   ;; Optional customizations
-  (js-indent-level 2)
+  (js-indent-level 4)
   ;; (typescript-ts-mode-indent-offset 2)
   ;; (jtsx-switch-indent-offset 0)
   ;; (jtsx-indent-statement-block-regarding-standalone-parent nil)
