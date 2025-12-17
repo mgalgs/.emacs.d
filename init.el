321fa9831199b1ee5c0950ba2a96fab3f593500c
Mitchel Humpherys <mitch.special@gmail.com>
2025-12-17 10:10:40 -0800

    init.el: consult: Enable consult-imenu-multi binding (`M-g I')

--- init.el
+++ init.el
@@ -1277,7 +1277,7 @@ eslint command line args with -c"
          ;; ("M-g m" . consult-mark)
          ;; ("M-g k" . consult-global-mark)
          ("M-g i" . consult-imenu)
-         ;; ("M-g I" . consult-imenu-multi)
+         ("M-g I" . consult-imenu-multi)
 
          ;; M-s bindings in `search-map'
          ;; ("M-s d" . consult-find)                  ;; Alternative: consult-fd
